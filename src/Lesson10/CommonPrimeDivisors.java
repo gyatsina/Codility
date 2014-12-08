65cf80ce0a174aa1fa6a6078f67746497bc96bce
Dahai Yao <729101421@qq.com>
2014-12-09 01:03:34 +0800

    Lesson12

--- src/Lesson10/CommonPrimeDivisors.java
+++ src/Lesson10/CommonPrimeDivisors.java
@@ -2,6 +2,70 @@ package Lesson10;
 
 /**
  * Created by yaodh on 2014/12/8.
+ * https://codility.com/demo/results/demoBGUQWU-F9P/
+ * 1. CommonPrimeDivisors
+ * Check whether two numbers have the same prime divisors.
+ * Task description
+ * A prime is a positive integer X that has exactly two distinct divisors: 1 and X. The first few prime integers are 2, 3, 5, 7, 11 and 13.
+ * A prime D is called a prime divisor of a positive integer P if there exists a positive integer K such that D * K = P. For example, 2 and 5 are prime divisors of 20.
+ * You are given two positive integers N and M. The goal is to check whether the sets of prime divisors of integers N and M are exactly the same.
+ * For example, given:
+ * N = 15 and M = 75, the prime divisors are the same: {3, 5};
+ * N = 10 and M = 30, the prime divisors aren't the same: {2, 5} is not equal to {2, 3, 5};
+ * N = 9 and M = 5, the prime divisors aren't the same: {3} is not equal to {5}.
+ * Write a function:
+ * class Solution { public int solution(int[] A, int[] B); }
+ * that, given two non-empty zero-indexed arrays A and B of Z integers, returns the number of positions K for which the prime divisors of A[K] and B[K] are exactly the same.
+ * For example, given:
+ * A[0] = 15   B[0] = 75
+ * A[1] = 10   B[1] = 30
+ * A[2] = 3    B[2] = 5
+ * the function should return 1, because only one pair (15, 75) has the same set of prime divisors.
+ * Assume that:
+ * Z is an integer within the range [1..6,000];
+ * each element of arrays A, B is an integer within the range [1..2147483647].
+ * Complexity:
+ * expected worst-case time complexity is O(Z*log(max(A)+max(B))2);
+ * expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+ * Elements of input arrays can be modified.
  */
 public class CommonPrimeDivisors {
+    public int solution(int[] A, int[] B) {
+        int ans = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (common(A[i], B[i])) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    private boolean common(int x, int y) {
+        int d = gcd(x, y);
+        return commonGCD(d, x) && commonGCD(d, y);
+    }
+
+    private boolean commonGCD(int x, int y) {
+        int d = gcd(x, y);
+        while (d != 1) {
+            y /= d;
+            d = gcd(x, y);
+        }
+        return x % y == 0;
+    }
+
+    private int gcd(int n, int m) {
+        int r = n % m;
+        while (r != 0) {
+            n = m;
+            m = r;
+            r = n % m;
+        }
+        return m;
+    }
+
+    public static void main(String[] args) {
+        int ans = new CommonPrimeDivisors().solution(new int[]{15, 10, 3}, new int[]{75, 30, 5});
+        System.out.println(ans);
+    }
 }
---
+++ src/Lesson12/MinMaxDivision.java
@@ -0,0 +1,107 @@
+package Lesson12;
+
+/**
+ * Created by yao on 2014/12/8.
+ * https://codility.com/demo/results/demoBXPEWQ-T6C/
+ * 1. MinMaxDivision
+ * Divide array A into K blocks and minimize the largest sum of any block.
+ * Task description
+ * You are given integers K, M and a non-empty zero-indexed array A consisting of N integers. Every element of the array is not greater than M.
+ * You should divide this array into K blocks of consecutive elements. The size of the block is any integer between 0 and N. Every element of the array should belong to some block.
+ * The sum of the block from X to Y equals A[X] + A[X + 1] + ... + A[Y]. The sum of empty block equals 0.
+ * The large sum is the maximal sum of any block.
+ * For example, you are given integers K = 3, M = 5 and array A such that:
+ * A[0] = 2
+ * A[1] = 1
+ * A[2] = 5
+ * A[3] = 1
+ * A[4] = 2
+ * A[5] = 2
+ * A[6] = 2
+ * The array can be divided, for example, into the following blocks:
+ * [2, 1, 5, 1, 2, 2, 2], [], [] with a large sum of 15;
+ * [2], [1, 5, 1, 2], [2, 2] with a large sum of 9;
+ * [2, 1, 5], [], [1, 2, 2, 2] with a large sum of 8;
+ * [2, 1], [5, 1], [2, 2, 2] with a large sum of 6.
+ * The goal is to minimize the large sum. In the above example, 6 is the minimal large sum.
+ * Write a function:
+ * class Solution { public int solution(int K, int M, int[] A); }
+ * that, given integers K, M and a non-empty zero-indexed array A consisting of N integers, returns the minimal large sum.
+ * For example, given K = 3, M = 5 and array A such that:
+ * A[0] = 2
+ * A[1] = 1
+ * A[2] = 5
+ * A[3] = 1
+ * A[4] = 2
+ * A[5] = 2
+ * A[6] = 2
+ * the function should return 6, as explained above. Assume that:
+ * N and K are integers within the range [1..100,000];
+ * M is an integer within the range [0..10,000];
+ * each element of array A is an integer within the range [0..M].
+ * Complexity:
+ * expected worst-case time complexity is O(N*log(N+M));
+ * expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+ * Elements of input arrays can be modified.
+ */
+public class MinMaxDivision {
+    public int solution(int K, int M, int[] A) {
+        int n = A.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += A[i];
+        }
+        int left = M, right = sum, ans = sum;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            int intervals = countIntervals(A, mid);
+            if(intervals == K) {
+                ans = Math.min(ans, mid);
+            }
+            if (intervals > K) {
+                left = mid + 1;
+            } else right = mid - 1;
+        }
+        return ans;
+    }
+
+    private int countIntervals(int[] A, int target) {
+        int sum = 0, count = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            if (sum > target) {
+                count++;
+                sum = A[i];
+            }
+        }
+        return count + (sum > 0 ? 1 : 0);
+    }
+
+    // 递归求解，TLE
+    // https://codility.com/demo/results/demoBXPEWQ-T6C/
+    /*public int solution(int K, int M, int[] A) {
+        int n = A.length;
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + A[i];
+        }
+        return bSum(sum, K, 0, n - 1);
+    }
+
+    private int bSum(int[] sum, int K, int start, int end) {
+        if (start > end) return 0;
+        if (K == 1) return sum[end + 1] - sum[start];
+        int ans = Integer.MAX_VALUE;
+        for (int i = start; i <= end + 1; i++) {
+            int left = bSum(sum, K / 2, start, i - 1);
+            int right = bSum(sum, K - K / 2, i, end);
+            ans = Math.min(ans, Math.max(left, right));
+        }
+        return ans;
+    }*/
+
+    public static void main(String[] args) {
+        int ans = new MinMaxDivision().solution(3, 5, new int[]{2, 1, 5, 1, 2, 2, 2});
+        System.out.println(ans);
+    }
+}
